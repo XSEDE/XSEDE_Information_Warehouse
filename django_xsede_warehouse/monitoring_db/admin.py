71ad54c6b17d0c87d05ed01898d95cf8b0adb4c6
JP Navarro <jpfnavarro@gmail.com>
2017-01-11 14:18:40 +0000

    Display Result in list view
    git-svn-id: https://software.xsede.org/svn/xci/source/info.warehouse/trunk@3447 1370f987-b873-4e1f-8c66-8b3e5403fd71

--- django_xsede_warehouse/monitoring_db/admin.py
+++ django_xsede_warehouse/monitoring_db/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from monitoring_db.models import *
 
 class TestResultAdmin(admin.ModelAdmin):
-    list_display = ('ResourceID', 'Name', 'ID', 'CreationTime')
+    list_display = ('ResourceID', 'Name', 'Result', 'ID', 'CreationTime')
     list_display_links = ['ID']
 
 # Register your models here.
