510a260fd736ac857301f0b5a95ac6dfb26e4bf1
JP Navarro <jpfnavarro@gmail.com>
2017-01-26 12:26:45 +0000

    Add the migrations also
    git-svn-id: https://software.xsede.org/svn/xci/source/info.warehouse/trunk@3513 1370f987-b873-4e1f-8c66-8b3e5403fd71

--- django_xsede_warehouse/outages/models.py
+++ django_xsede_warehouse/outages/models.py
@@ -5,7 +5,7 @@ import jsonfield
 
 class Outages(models.Model):
     #id = models.AutoField(primary_key=True, null=False)
-#OutageID,ResourceID,WebURL,Subject,Content,OutageStart,OutageEnd,SiteID
+    #OutageID,ResourceID,WebURL,Subject,Content,OutageStart,OutageEnd,SiteID
     OutageID = models.IntegerField(primary_key=True, null=False)
     ResourceID = models.CharField(max_length=64)
     WebURL = models.CharField(max_length=320)
