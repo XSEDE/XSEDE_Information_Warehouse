228188aee43158cc0822a458ffa49018afbcfc20
JP Navarro <jpfnavarro@gmail.com>
2017-07-06 20:21:22 +0000

    Update reset
    git-svn-id: https://software.xsede.org/svn/xci/source/info.warehouse/trunk@3813 1370f987-b873-4e1f-8c66-8b3e5403fd71

--- django_xsede_warehouse/devtools/reset.sql
+++ django_xsede_warehouse/devtools/reset.sql
@@ -4,3 +4,5 @@ create schema django;
 alter schema django owner to django_owner;
 create schema glue2;
 alter schema glue2 owner to glue2_owner;
+create schema xcsr;
+alter schema xcsr owner to xcsr_owner;
