953b4651d332dc6f73fd082696e6983389f62a5e
JP Navarro <jpfnavarro@gmail.com>
2019-01-16 15:39:00 +0000

    Make devtools/dumpall.sh work on awsadmin
    git-svn-id: https://software.xsede.org/svn/xci/source/info.warehouse/trunk@4756 1370f987-b873-4e1f-8c66-8b3e5403fd71

--- django_xsede_warehouse/devtools/dump_all.sh
+++ django_xsede_warehouse/devtools/dump_all.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-/usr/pgsql-9.4/bin/pg_dump -U django_owner -n django -d warehouse \
+DBHOST=information-warehouse-prod-cluster.cluster-clabf5kcvwmz.us-east-2.rds.amazonaws.com
+
+/usr/pgsql-9.4/bin/pg_dump -h ${DBHOST} -U django_owner -n django -d warehouse \
   >django.dump
 
-/usr/pgsql-9.4/bin/pg_dump -U xcsr_owner -n xcsr -d warehouse \
+/usr/pgsql-9.4/bin/pg_dump -h ${DBHOST} -U xcsr_owner -n xcsr -d warehouse \
   >xcsr.dump
 
-/usr/pgsql-9.4/bin/pg_dump -U glue2_owner -n glue2 -d warehouse --exclude-table-data=glue2_db_entityhistory \
+/usr/pgsql-9.4/bin/pg_dump -h ${DBHOST} -U glue2_owner -n glue2 -d warehouse --exclude-table-data=glue2_db_entityhistory \
   >glue2.dump
 
